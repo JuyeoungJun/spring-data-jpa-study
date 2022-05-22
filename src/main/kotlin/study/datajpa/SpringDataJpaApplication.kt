59bb8b02d8effa29d7e774ed04354805a32cd930
lucas(주영준) <lucas.0514@kakaocorp.com>
2022-05-22 19:52:01 +0900

    first commit

---
+++ src/main/kotlin/study/datajpa/SpringDataJpaApplication.kt
@@ -0,0 +1,11 @@
+package study.datajpa
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringDataJpaApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringDataJpaApplication>(*args)
+}
